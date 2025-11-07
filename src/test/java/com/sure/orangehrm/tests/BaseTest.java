393946ea095143c94d6b43f076eba215244bf0b5
khaledzaky <khaledzaky07@gmail.com>
2025-11-08 01:48:01 +0200

    feat: initialize BaseTest

---
+++ src/test/java/com/sure/orangehrm/tests/BaseTest.java
@@ -0,0 +1,19 @@
+package com.sure.orangehrm.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
