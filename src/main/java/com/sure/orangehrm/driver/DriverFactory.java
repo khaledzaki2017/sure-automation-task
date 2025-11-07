2a41f13fb997e6357b560f5b1875d9350b2ccba5
khaledzaky <khaledzaky07@gmail.com>
2025-11-07 22:51:12 +0200

    feat:Add automatic ChromeDriver setup using WebDriverManager

--- src/main/java/com/sure/orangehrm/driver/DriverFactory.java
+++ src/main/java/com/sure/orangehrm/driver/DriverFactory.java
@@ -1,16 +1,26 @@
 package com.sure.orangehrm.driver;
 
 import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class DriverFactory {
 
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
 
-    public static void setDriver(WebDriver driver) {
-        DRIVER.set(driver);
-    }
+//    public static void setDriver(WebDriver driver) {
+//        DRIVER.set(driver);
+//    }
 
     public static WebDriver getDriver() {
+        if( DRIVER.get()==null){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options=new ChromeOptions();
+            options.addArguments("--start-maximized");
+            DRIVER.set(new ChromeDriver(options));
+        }
         return DRIVER.get();
     }
 
