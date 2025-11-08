6c78100d0dfc20cd8ac9133ae4653bebfe23f749
khaledzaky <khaledzaky07@gmail.com>
2025-11-08 02:50:35 +0200

    refactor:Add logging and exception handling

--- src/test/java/com/sure/orangehrm/tests/BaseTest.java
+++ src/test/java/com/sure/orangehrm/tests/BaseTest.java
@@ -1,19 +1,35 @@
-package com.company.orangehrm.tests;
+package com.sure.orangehrm.tests;
 
-import com.company.orangehrm.driver.DriverFactory;
+import com.sure.orangehrm.driver.DriverFactory;
+import com.sure.orangehrm.utils.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
 public class BaseTest {
     protected WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = DriverFactory.getDriver();
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        try {
+            String headless = ConfigReader.get("headless");
+            System.setProperty("headless", headless);
+            System.out.println("[BaseTest] Running in headless mode: " + headless);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to set headless mode", e);
+        }
     }
 
-    @AfterMethod
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        driver = DriverFactory.getDriver();
+        System.out.println("[BaseTest] WebDriver started successfully");
+    }
+
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        DriverFactory.quitDriver();
+        if (driver != null) {
+            DriverFactory.quitDriver();
+            System.out.println("[BaseTest] WebDriver closed successfully");
+        }
     }
 }
