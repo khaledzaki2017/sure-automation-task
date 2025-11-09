ffbacb60247823718804d226d7da6dcd52c3a26c
khaledzaky <khaledzaky07@gmail.com>
2025-11-09 19:29:16 +0200

    feat(api):add the basic E2E test

---
+++ src/test/java/com/sure/orangehrm/tests/E2ETest.java
@@ -0,0 +1,17 @@
+package com.sure.orangehrm.tests;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class E2ETest extends BaseTest {
+
+    @BeforeClass
+    public void setup() {
+        System.out.println("Setup test data here later...");
+    }
+
+    @Test
+    public void verifyEmployeeE2EFlow() {
+        System.out.println("Will implement UI + API flow later...");
+    }
+}
