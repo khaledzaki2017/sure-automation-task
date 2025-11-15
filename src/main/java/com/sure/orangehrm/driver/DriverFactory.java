d34d9f1152d845e30b3a7d4dffa3ab46441e512f
khaledzaky <khaledzaky07@gmail.com>
2025-11-15 16:22:49 +0200

    refactor: add createChromeOptions method in Driver factory to manage options for headless mode also

--- src/main/java/com/sure/orangehrm/driver/DriverFactory.java
+++ src/main/java/com/sure/orangehrm/driver/DriverFactory.java
@@ -2,36 +2,45 @@ package com.sure.orangehrm.driver;
 
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
 
-//    public static void setDriver(WebDriver driver) {
-//        DRIVER.set(driver);
-//    }
-
     public static WebDriver getDriver() {
-        if( DRIVER.get()==null){
+        if (DRIVER.get() == null) {
             WebDriverManager.chromedriver().setup();
-            ChromeOptions options=new ChromeOptions();
-            options.addArguments("--lang=en-US");
-            options.addArguments("--accept-lang=en-US");
-            options.addArguments("--disable-features=TranslateUI");
-            options.addArguments("--disable-translate");
-
-            // Clear cache and cookies
-            options.addArguments("--disable-application-cache");
-            options.addArguments("--disable-cache");
-
-            if (isHeadless()) options.addArguments("--headless=new");
-            options.addArguments("--start-maximized");
+            ChromeOptions options = createChromeOptions();
             DRIVER.set(new ChromeDriver(options));
         }
         return DRIVER.get();
+    }
+
+    private static ChromeOptions createChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+
+        // General settings (common for headless + normal)
+        options.addArguments("--lang=en-US");
+        options.addArguments("--accept-lang=en-US");
+        options.addArguments("--disable-features=TranslateUI");
+        options.addArguments("--disable-translate");
+        options.addArguments("--disable-application-cache");
+        options.addArguments("--disable-cache");
+
+        if (isHeadless()) {
+            System.out.println("[Headless mode] Tests are running in headless mode");
+            options.addArguments("--headless=new");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        } else {
+            options.addArguments("--start-maximized");
+        }
+
+        return options;
     }
 
     public static void quitDriver() {
@@ -40,6 +49,7 @@ public class DriverFactory {
             DRIVER.remove();
         }
     }
+
     private static boolean isHeadless() {
         return "true".equalsIgnoreCase(System.getProperty("headless", "false"));
     }
